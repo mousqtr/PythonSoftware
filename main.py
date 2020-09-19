9083b9f7f76e1031645fdee44fcffb105114a84d
mousqtr <49991720+mousqtr@users.noreply.github.com>
2020-09-19 03:01:43 +0200

    Add icon, login buttons

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ window_width = 1000
 window_height = 700
 title_font = ("Calibri bold", 16)
 menu_font = ("Calibri bold", 14)
+low_font = ("Calibri bold", 13)
 
 
 # Root initialization
@@ -38,19 +39,18 @@ root.columnconfigure(0, weight=1)
 # Left menu
 frame_left_width = window_width/5
 frame_left = tk.Frame(root, bg=color_left_menu, width=frame_left_width)
-frame_left.grid(column=0, sticky='news')
+frame_left.grid(row=0, column=0, sticky='news')
 frame_left.columnconfigure(0, weight=1)
 
-# Main frame
-frame_main_width = 4*(window_width/5)
-frame_main = tk.Frame(root, bg=color_window, width=frame_main_width)
-frame_main.grid(column=1, sticky='news')
-frame_main.grid_rowconfigure(0, weight=1)
-frame_main.columnconfigure(0, weight=1)
+# Right frame
+frame_right_width = 4*(window_width/5)
+frame_right = tk.Frame(root, bg="yellow", width=frame_right_width)
+frame_right.grid(row=0, column=1, sticky='news')
+frame_right.columnconfigure(0, weight=1)
 
 # Left title
-label_title = tk.Label(frame_left, text="Nom entreprise", bg="#DCDCDC", fg="black")
-label_title.grid(row=0, sticky='new', pady=(0,20))
+label_title = tk.Label(frame_left, text="Nom entreprise", bg="purple", fg="white")
+label_title.grid(row=0, sticky='new', pady=(0, 20))
 label_title.config(font=title_font)
 
 
@@ -71,10 +71,56 @@ class Button_menu():
 
 Button_1 = Button_menu("Tableau de bord", 1)
 Button_2 = Button_menu("Produits", 2)
-Button_3 = Button_menu("Historiques", 3)
+Button_3 = Button_menu("Historique", 3)
 
 
+# Top menu
+frame_top_menu = tk.Frame(frame_right, bg="#dbdbdb", width=frame_right_width, height=32)
+frame_top_menu.grid(row=0, sticky='new')
+frame_top_menu.columnconfigure(0, weight=1)
 
+# Top menu - right icon
+settings_icon = tk.PhotoImage(file="settings.png")
+button_setting = tk.Button(frame_top_menu, image=settings_icon, height=24, width=24, borderwidth=0)
+button_setting.grid(row=0, column=0, sticky="e", padx=(2,2), pady=(2,2))
+
+
+# Top menu - right user name
+def on_enter(e):
+    frame_top_menu['bg'] = 'orange'
+    frame_top_menu['fg'] = 'white'
+
+
+def on_leave(e):
+    frame_top_menu['bg'] = "#dbdbdb"
+    frame_top_menu['fg'] = 'black'
+
+
+def create_window():
+    login_window = tk.Toplevel(root)
+    login_window_width = 500
+    login_window_height = 200
+    x_cord = int((screen_width / 2) - (login_window_width / 2))
+    y_cord = int((screen_height / 2) - (login_window_height / 2))
+    login_window.geometry("{}x{}+{}+{}".format(login_window_width, login_window_height, x_cord, y_cord))
+
+
+    b4 = tk.Button(login_window, text=" ")
+    b4.grid(row=1, column=3, sticky="e")
+
+frame_top_menu = tk.Button(frame_top_menu, text="Se connecter", bg="#dbdbdb", borderwidth=0, command=create_window)
+frame_top_menu.grid(row=0, column=1, sticky="e", padx=(10,5))
+frame_top_menu.config(font=low_font)
+frame_top_menu.bind("<Enter>", on_enter)
+frame_top_menu.bind("<Leave>", on_leave)
+
+# Top horizontal bar
+frame_main = tk.Frame(frame_right, bg="black", width=frame_right_width, height=2)
+frame_main.grid(row=1, sticky='new')
+
+# Main frame
+frame_main = tk.Frame(frame_right, bg="#e8e8e8", width=frame_right_width, height=window_height-34)
+frame_main.grid(row=2, sticky='new')
 
 
 # # Main title
